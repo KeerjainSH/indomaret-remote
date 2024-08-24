8ab7016d0b5907625be376f94f0b89c383f7802e
Eka Rahadi <ekarahadi98@gmail.com>
2024-08-24 18:52:23 +0800

    feat: add composer

---
+++ services/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'scripts/ssh_helper.php';
+
+$ssh = SSHConnection::getInstance('4.145.89.179', 22, 'eka_rahadi');
+
+// echo file_get_contents('./scripts/id_ed25519');
+$output = $ssh->executeCommand('dir');
+echo nl2br($output); //Printf
+?>
